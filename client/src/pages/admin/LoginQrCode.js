0d000dfcf5a5ab7867c0031be0be38e016f001b9
13-Kavisurya <kavisbalas13@gmail.com>
2026-04-07 22:08:58 +0530

    Remove unused useEffect import and disable JSX comment linting

--- client/src/pages/admin/LoginQrCode.js
+++ client/src/pages/admin/LoginQrCode.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import QRCode from 'react-qr-code';
 import { FiSmartphone, FiEdit2, FiRepeat } from 'react-icons/fi';
