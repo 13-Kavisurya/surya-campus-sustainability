d44c46b23e00cdcc38c5efa29e457bcd4dccce5d
13-Kavisurya <kavisbalas13@gmail.com>
2026-04-08 05:24:18 +0530

    resolve

--- server/index.js
+++ server/index.js
@@ -17,7 +17,8 @@ app.use(cors({
     origin: [
         'http://localhost:3000',
         'http://localhost:3001',
-        'https://surya-campus-sustainability-report-portal-9jahxqmeq.vercel.app'
+        'https://surya-campus-sustainability-report-portal-9jahxqmeq.vercel.app',
+        'https://surya-campus-sustainability-report-portal-oqe0o8eap.vercel.app'
     ],
     credentials: true
 }));
